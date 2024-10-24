7b25a55634d7d7f1107254e4d631b3b14dae94a4
enj0yer <kulagin2002ilya@gmail.com>
2024-10-24 11:16:18 +0000

    Add helper function

--- src/Helpers/functions.php
+++ src/Helpers/functions.php
@@ -6,3 +6,14 @@ function isEntityContainsProperty($entity, $propertyName): bool
 {
     return !empty($entity->{$propertyName});
 }
+
+function assocArrayByKey(array $array, $keyToExtract = 'id'): array
+{
+    $new = [];
+    foreach ($array as $item) {
+        if (array_key_exists($keyToExtract, $item)) {
+            $new[$item[$keyToExtract]] = $item;
+        }
+    }
+    return $new;
+}
